4d3986daf856e87b6e02219ab9e3b64aeba74ad4
Vimal Raj <7770vijayan@gmail.com>
2025-01-26 22:26:27 +0530

    finished

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -44,10 +44,10 @@ const Home = () => {
             <View className="flex justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
+                  Welcome Back To
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  JSMastery
+                  Aora
                 </Text>
               </View>
 
